e1cbe22897dda6aca1f94b7dcf6257f4e527d5e9
aliasgharkarani <aliasgharkarani125@hotmail.com>
2019-11-28 17:29:25 +0500

    Emit Event from child to parent with emitters

--- src/app/child-comp/child-comp.component.ts
+++ src/app/child-comp/child-comp.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child-comp',
   templateUrl: './child-comp.component.html',
   styleUrls: ['./child-comp.component.scss']
 })
-export class ChildCompComponent implements OnInit {
+export class ChildCompComponent implements OnInit, OnChanges {
   @Input() parentEmail: string;
   prop !: number;
-
+  mail: any = '';
+  @Output() emailOutPut = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
-    console.warn(this.parentEmail);
+    // console.warn(this.parentEmail);
+    // this.emailOutPut.emit('alifrom child');
   }
-  // ngOnChanges(changes: SimpleChanges) {
-  //   console.log(changes);
-  //   // console.warn(this.parentEmail);
-  // }
+  ngOnChanges(changes: SimpleChanges) {
+    // console.log(changes);
+    // this.emailOutPut.emit(this.mail);
+    this.mail = this.parentEmail;
+    // console.warn(this.parentEmail, 'wow');
+  }
 }
--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, OnChanges, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, FormArray, FormBuilder } from '@angular/forms';
 // import { ChildComponent   } from '../child-comp/child-comp.component';
 
@@ -10,21 +10,30 @@ import { FormControl, FormGroup, FormArray, FormBuilder } from '@angular/forms';
 export class NameEditorComponent implements OnInit {
   form: FormGroup;
   email: any = '';
+
+
+  // @Output() parentEmail: string;
+
   constructor(private fb: FormBuilder) {
   }
+  // ngOnChanges(changes: SimpleChanges) {
+  //   // console.log(changes);
+  //   console.warn(this.parentEmail, 'wow');
+  // }
   ngOnInit() {
-    this.form = this.fb.group({
-      credentials: this.fb.array([]),
-    });
+    // this.form = this.fb.group({
+    //   credentials: this.fb.array([]),
+    // });
   }
-  onClickM() {
-    console.warn(this.form.value.credentials, 'wow');
+  onClickM(event) {
+    console.log(event, 'emitter');
+    // console.warn(this.form.value.credentials, 'wow');
   }
-  addCreds() {
-    const creds = this.form.get('credentials') as FormArray;
-    creds.push(this.fb.group({
-      username: 'ali',
-      todo: 'work',
-    }));
-  }
+  // addCreds() {
+  //   const creds = this.form.get('credentials') as FormArray;
+  //   creds.push(this.fb.group({
+  //     username: 'ali',
+  //     todo: 'work',
+  //   }));
+  // }
 }
